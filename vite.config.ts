9b5b5375da9610f04261a5bb49af4630354eb4f1
YaraOduvanchik <Cherashev4@gmail.com>
2025-05-12 16:18:50 +0500

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/PetHelperWeb/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
+	plugins: [react(), tailwindcss()],
+	base: "/PetHelperWeb/",
 });
